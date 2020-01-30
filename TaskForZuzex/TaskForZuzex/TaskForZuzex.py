81ab889a885a3b4c13f17645f78dfb7f2f5d44ba
Ma4ete-S <Sergeimer_ivanov@mail.ru>
2020-01-30 20:45:15 +0300

    Second commit
    Добавлена функция получения разделительных линий.
    Добавлена функция меняющая местами длину и ширну согласно условий алгоритма.
    Убран мусор.
    В работе: кластеризация

--- TaskForZuzex/TaskForZuzex/TaskForZuzex.py
+++ TaskForZuzex/TaskForZuzex/TaskForZuzex.py
@@ -7,36 +7,46 @@ import os
 def get_image(path):
     if not os.path.exists(path):
         print_error('File does not exists.')
-    image = cv.imread(path)
-    if image==None:
+    if not cv.haveImageReader(path):
         print_error('File has no image or is corrupted.')
     else: 
-        return image
-
-    
+        return cv.imread(path)
 
 def create_rotated_rect(image):
-    width = np.size(image,0) 
-    height = np.size(image,1)
+    height = np.size(image,0) 
+    width = np.size(image,1) 
     area = width * height
-    _delta = 3
+    _delta = 10
     _min_area = int(area * 0.0002)
-    _max_area = int(area * 0.05)
+    _max_area = int(area * 0.03)
     mser = cv.MSER_create(_delta,_min_area,_max_area)
     image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    image_gray = cv.medianBlur(image_gray,5)
-    grayscale_levels(image_gray)
+    #ДОБАВИТЬ УЛУЧШЕНИЕ + КОНТРАСТНОСТЬ
     regions, _ = mser.detectRegions(image_gray)
     hulls = [cv.convexHull(p.reshape(-1, 1, 2)) for p in regions]
 
-    if len(hulls)<5:
+    if len(hulls)<10:
         print_error("Image have not consist any barcode.")
 
     rotated_rect = []
     for i, contour in enumerate(hulls):
-        rotated_rect.append(cv.minAreaRect(contour))
+        rect = cv.minAreaRect(contour)
 
-    return rotated_rect
+        if rect[1][0] != 0 and rect[1][1] != 0:
+            if rect[1][0]/rect[1][1]>5 or rect[1][1]/rect[1][0]>5:
+                if rect[1][1]>rect[1][0]:
+                    revers_rect = [0,1,2]
+                    revers_rect[0] = rect[0]
+                    revers_rect[1] = list(rect[1])
+                    revers_rect[1][0], revers_rect[1][1] = revers_rect[1][1], revers_rect[1][0]
+                    revers_rect[1] = tuple(revers_rect[1])
+                    revers_rect[2] += rect[2] + 90
+                    rotated_rect.append(tuple(revers_rect))
+                else:   
+                    rotated_rect.append(rect)
+            
+    sorted_areas = sorted(rotated_rect, key = lambda left_center_point:rotated_rect[0][0])
+    return tuple(sorted_areas)
 
 def print_error(description):
     print(description)
@@ -44,29 +54,10 @@ def print_error(description):
 
 def view_image(image):
     cv.namedWindow('Display', cv.WINDOW_NORMAL)
+    cv.resizeWindow('Display',np.size(image,1) ,np.size(image,0))
     cv.imshow('Display', image)
     cv.waitKey(0)
     cv.destroyAllWindows()
-
-def grayscale_levels(image):
-    high = 255
-    while(1):
-        low = high - 5
-        col_to_be_changed_low = np.array([low])
-        col_to_be_changed_high = np.array([high]) 
-        lvl_mask = cv.inRange(image,col_to_be_changed_low,col_to_be_changed_high)
-        image[lvl_mask>0] = (high)
-        high -= 5
-        if (low <= 0):
-            break
-    return image
-
-def rect_filter(rotated_rect):
-    filtered_rect = []
-    for i, element in enumerate(rotated_rect):
-        if element[1][0]/element[1][1]>5 or element[1][1]/element[1][0]>5:
-            filtered_rect.append(element)
-    return filtered_rect
 
 def create_split_line(rect):
     split_lines = []
@@ -74,48 +65,40 @@ def create_split_line(rect):
         centr_point = (int(element[0][0]),int(element[0][1]))
         width = element[1][0] 
         height = element[1][1]
-        a = element[2]
-        dx = math.sin(a+90)
-        dy = math.cos(a+90)
-       
-        if height>width:
-            l = 0.3 * height
-            point_bis_1 = (int(element[0][0]+dx*l),int(element[0][1]+dy*l))
-            point_bis_2 = (int(element[0][0]-dx*l),int(element[0][1]-dy*l))
-            cv.line(image,centr_point,point_bis_1,(0,255,0),1)
-            cv.line(image,centr_point,point_bis_2,(0,255,0),1)
-        else: 
-            l = 0.3 * width
-            point_bis_1 = (int(element[0][0]+dx*l),int(element[0][1]-dy*l))
-            point_bis_2 = (int(element[0][0]-dx*l),int(element[0][1]+dy*l))
-            cv.line(image, centr_point, point_bis_1, (0,255,0))
-            cv.line(image, centr_point, point_bis_2, (0,255,0))
-        
+        a = math.radians(abs(element[2]))
+        l = 0.3 * height
+        dx = math.cos(a)*l
+        dy = math.sin(a)*l
+
+               
+        point_split_1 = (int(centr_point[0] + dx), int(centr_point[1] - dy))
+        point_split_2 = (int(centr_point[0] - dx), int(centr_point[1] + dy))
             
+        cv.line(image, point_split_1, point_split_2, (0,255,0))
+
+        a = element[2]; 
+        add = (point_split_1,point_split_2,a)  
+        split_lines.append(add)
+    return tuple(split_lines)     
 
 
-        #bisectors.append(cv.minAreaRect(element))
+path = 'C:/Photo/5.png'
 
-path = 'C:/Photo/NVIDIA_SLA_cuDNN_Support.txt'
+
 image = get_image(path)
-
-
 rotated_rect = create_rotated_rect(image)
-filtred_rect = rect_filter(rotated_rect)
-split_lines = create_split_line(filtred_rect)
+split_lines = create_split_line(rotated_rect)
 
 
 
-box = np.array(len(filtred_rect)) 
-box = np.int0(filtred_rect)
-
-for i, contour in enumerate(filtred_rect):
-    box.append(cv.boxPoints(filtred_rect[i]))
+box = []
+for i, contour in enumerate(rotated_rect):
+    print(rotated_rect[i])
+    box.append(cv.boxPoints(rotated_rect[i]))
     box[i] = np.int0(box[i])
-
 
 for i, contour in enumerate(box):    
     cv.drawContours(image,[contour],0,(0,0,255),1)
 
-print(rotated_rect[0])
+
 view_image(image)
---
+++ TaskForZuzex/TaskForZuzex/Trash.py
@@ -0,0 +1,49 @@
+
+    #image_gray = cv.medianBlur(image_gray,5)
+    #ДОБАВИТЬ УЛУЧШЕНИЕ + КОНТРАСТНОСТЬ
+    # grayscale_levels(image_gray)
+
+
+ #   def grayscale_levels(image):
+ #   high = 255
+ #   while(1):
+  #      low = high - 10
+  #      col_to_be_changed_low = np.array([low])
+   #     col_to_be_changed_high = np.array([high]) 
+   #     lvl_mask = cv.inRange(image,col_to_be_changed_low,col_to_be_changed_high)
+   #     image[lvl_mask>0] = (high)
+   #     high -= 10
+   #     if (low <= 0):
+   #         break
+   # return image
+
+
+#def rect_filter(rotated_rect):
+#   filtered_rect = []
+#    for i, element in enumerate(rotated_rect):
+#        if element[1][0]/element[1][1]>5 or element[1][1]/element[1][0]>5:
+#            filtered_rect.append(element)
+#    return tuple(filtered_rect)
+
+
+
+        
+        #else: 
+        #    l = 0.3 * width
+        #    point_split_1 = (int(centr_point[0] + math.sin(a)*l), int(centr_point[1] + math.cos(a)*l))
+        #    point_split_2 = (int(centr_point[0] - math.sin(a)*l), int(centr_point[1] - math.cos(a)*l))
+        #    a = abs(element[2])
+            
+        #    cv.line(image, point_split_1, point_split_2, (0,255,0))
+
+
+        #def union_of_intersecting_areas(area,center_line):
+   # for i, line in enumerate(center_line):    
+     #   if center_line[i][2]>=line[2]-2 and center_line[i][2]<=line[2]+2:
+     #       if area[i][0][0]+
+
+
+    #for i, element in enumerate(sorted_areas):
+    #    if element[0][1]<sorted_areas[i][0][1]-sorted_areas[i][1][1]
+
+#filtred_rect = rect_filter(rotated_rect)
